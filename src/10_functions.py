475374b6fe974604075cc8cf366acf29f5139e23
Erik Rodriguez <eric19.21.03@gmail.com>
2020-06-02 20:08:23 -0700

    Adds functions on file 10 and Adds more functions on file 11

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,6 +1,12 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
+def is_even(number):
+    if num % 2 == 0:
+        return print(f"{number} is Even!")
+    else:
+        return print(f"{number} is Odd!")
+
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
@@ -9,4 +15,4 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
-
+is_even(num)
--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,9 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(num1, num2):
+    return num1 + num2
+
 
 print(f1(1, 2))
 
@@ -13,6 +16,22 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+
+
+def f2(*num):
+    sum = 0
+
+    print(type(num))
+
+    if type(num) == tuple:
+        for i in num:
+            sum = sum + i
+    elif type(num) == list:
+        for i in range(len(num)):
+            sum = sum + num[i]
+
+    return sum
+
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -31,6 +50,11 @@ print(f2(a))    # Should print 22
 
 # YOUR CODE HERE
 
+
+def f3(num1=1, num2=1):
+    return num1 + num2
+
+
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
@@ -44,6 +68,9 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kwargs):
+    return print(f"key: {kwargs.key()}, value: {kwargs.value()}")
+
 
 # Should print
 # key: a, value: 12
